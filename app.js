e3060432f0885644d19e9732ca26bcc233c0b4c8
Raghulal Prajapati <56857544+prajapatiraghulal@users.noreply.github.com>
2021-09-18 22:44:27 +0530

    gui designed

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const ejs = require('ejs');
+
+const app = express();
+
+
+app.set('view engine', 'ejs');
+app.use(express.static("public"));
+
+
+app.get("/", (req, res) => {
+    res.render('index.ejs');
+});
+
+app.listen(3000, () => {
+    console.log("listening on port 3000");
+})
