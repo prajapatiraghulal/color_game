a86f377ff9361c0d256501c1ab78c8f116a33623
Raghulal Prajapati <56857544+prajapatiraghulal@users.noreply.github.com>
2021-09-19 21:58:11 +0530

    ready working game without login/signup

--- app.js
+++ app.js
@@ -1,16 +1,54 @@
 const express = require('express');
+const bodyParser = require('body-parser')
 const ejs = require('ejs');
-
+const Math = require('mathjs');
 const app = express();
 
-
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 app.set('view engine', 'ejs');
 app.use(express.static("public"));
 
 
+
+const data = {
+    difficulty: 6,
+    color: [],
+
+}
+
+
+function rand_color(dat) {
+    dat.color = [];
+
+    for (var i = 0; i < dat.difficulty; ++i) {
+        dat.color.push(`rgb(${Math.floor(Math.random()*256)}, ${Math.floor(Math.random()*256)}, ${Math.floor(Math.random()*256)})`);
+    }
+}
+
 app.get("/", (req, res) => {
-    res.render('index.ejs');
+
+    rand_color(data);
+    res.render('index.ejs', { data });
 });
+app.post("/", (req, res) => {
+
+    if (req.body.reset_btn == "Reset") {
+
+        rand_color(data);
+        res.render("index.ejs", { data });
+    }
+    if (req.body.difficulty_btn == "difficulty") {
+        if (data.difficulty == 3) {
+            data.difficulty = 6;
+        } else {
+            data.difficulty = 3;
+        }
+        req.body.difficulty_btn = undefined;
+        rand_color(data);
+        res.render("index.ejs", { data });
+    }
+})
 
 app.listen(3000, () => {
     console.log("listening on port 3000");
